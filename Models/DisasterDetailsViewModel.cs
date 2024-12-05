81da3def182548c0d0f74d3375f0a8256e93ad9b
Yoanna Ruseva <115207755+YoannaR3@users.noreply.github.com>
2024-12-05 14:56:40 +0200

    Update DisasterDetailsViewModel.cs

--- Models/DisasterDetailsViewModel.cs
+++ Models/DisasterDetailsViewModel.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public required string Name { get; set; }
         public required string Description { get; set; }
+        public required string Description { get; set; }
         public required string Start { get; set; }
         public required string End { get; set; }
         public required string CreatedOn { get; set; }
